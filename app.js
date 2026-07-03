842509ce37729668c61b97371753e8dc796d9ab9
kr0k0diL21 <artem.walser@gmail.com>
2026-07-03 19:54:30 +0200

    feat: complete core gameplay loop (jump + collisions + entity movement)

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let gameState = {
 let player = {
   element: document.getElementById('mario'),
   x: 50,
-  y: 300,
+  y: 340,
   width: 20,
   height: 20,
   velocityX: 0,
@@ -56,8 +56,8 @@ const levels = [
       { x: 320, y: 220 },
       { x: 620, y: 260 },
     ],
-    surpriseBlocks: [{ x: 350, y: 220, type: 'mushroom' }],
-    pipes: [{ x: 750, y: 300 }],
+    surpriseBlocks: [{ x: 355, y: 235, type: 'mushroom' }],
+    pipes: [{ x: 750, y: 320 }],
   },
   //level 2
   {
@@ -109,7 +109,7 @@ function loadLevel(levelIndex) {
 
   //Reset player position
   player.x = 50;
-  player.y = 300;
+  player.y = 340;
   player.velocityX = 0;
   player.velocityY = 0;
   player.big = false;
@@ -151,15 +151,44 @@ function loadLevel(levelIndex) {
   });
   //Create coins
   level.coins.forEach((coinData, index) => {
-    const coins = createElement('div', 'coin', {
+    const coin = createElement('div', 'coin', {
       left: coinData.x + 'px',
       top: coinData.y + 'px',
     });
-    gameArea.appendChild(coins);
+    gameArea.appendChild(coin);
     gameObjects.coins.push({
-      element: coins,
+      element: coin,
       ...coinData,
       id: 'coin-' + index,
+      collected: false,
+    });
+  });
+  //Create surprise blocks
+  level.surpriseBlocks.forEach((blockData, index) => {
+    const block = createElement('div', 'surprise-block', {
+      left: blockData.x + 'px',
+      top: blockData.y + 'px',
+    });
+    gameArea.appendChild(block);
+    gameObjects.surpriseBlocks.push({
+      element: block,
+      ...blockData,
+      id: 'surprise-block-' + index,
+      hit: false,
+      type: blockData.type,
+    });
+  });
+  //Create pipes
+  level.pipes.forEach((pipeData, index) => {
+    const pipe = createElement('div', 'pipe', {
+      left: pipeData.x + 'px',
+      top: pipeData.y + 'px',
+    });
+    gameArea.appendChild(pipe);
+    gameObjects.pipes.push({
+      element: pipe,
+      ...pipeData,
+      id: 'pipe-' + index,
     });
   });
 }
@@ -220,6 +249,49 @@ function gameLoop() {
 }
 
 //Update game logic
-function update() {}
+function update() {
+  console.log(gameState.keys);
+  //Handle player movement
+  if (gameState.keys['ArrowLeft'] || gameState.keys['KeyA']) {
+    player.velocityX = -MOVE_SPEED;
+  } else if (gameState.keys['ArrowRight'] || gameState.keys['KeyD']) {
+    player.velocityX = MOVE_SPEED;
+  } else {
+    player.velocityX *= 0.8;
+  }
+  if (gameState.keys['Space'] && player.grounded) {
+    player.velocityY = JUMP_FORCE;
+    player.grounded = false;
+  }
+  if (!player.grounded) {
+    player.velocityY += GRAVITY;
+  }
+  player.x += player.velocityX;
+  player.y += player.velocityY;
+
+  //platform collision detection
+  player.grounded = false;
+  for (let platform of gameObjects.platforms) {
+    if (checkColision(player, platform)) {
+      if (player.velocityY > 0) {
+        player.y = platform.y - player.height;
+        player.velocityY = 0;
+        player.grounded = true;
+      }
+    }
+  }
+  updateElementPosition(player.element, player.x, player.y);
+}
+
+function checkColision(element1, element2) {
+  // Implementation for collision detection
+  return (
+    element1.x < element2.x + element2.width &&
+    element1.x + element1.width > element2.x &&
+    element1.y < element2.y + element2.height &&
+    element1.y + element1.height > element2.y
+  );
+}
+
 //Start the game
 initGame();
